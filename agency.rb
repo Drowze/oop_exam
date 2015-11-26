022dd54105a59304344d0382355bb6aad19dd0d1
Rafael Gibim <gibim6@gmail.com>
2015-11-26 18:49:56 +0000

    commit3

--- agency.rb
+++ agency.rb
@@ -1,7 +1,7 @@
 require_relative './curler.rb'
 
 class Agency
-  attr_reader :males, :females
+  attr_reader :males, :females, :pairs
 
   def initialize
     @males = Array.new
@@ -25,7 +25,20 @@ class Agency
     puts 'FEMALE CURLERS:'
     @females.each { |person| puts "#{person.name}'s proficiency is #{person.proficiency}. Seeks partner with proficiency >= #{person.desired_proficiency}."}
   end
+
+  def create_teams
+    @pairs = Hash.new
+    sorted_males = Array.new
+    sorted_females = Array.new
+
+    sorted_males = @males.sort { |a,b| b.proficiency <=> a.proficiency}
+    sorted_females = @females.sort { |a,b| b.proficiency <=> a.proficiency}
+    
+    for i in 0..sorted_males.size-1
+    end
+  end
 end
 
 obj = Agency.new
 obj.status
+obj.create_teams
